d2a8594f4144e3c8b1c6770d72eba72514a015ac
kbazilenko <skip48112@yandex.ru>
2026-05-27 19:12:14 +0300

    Fixed logger's dll-import warnigns

--- pandora/src/core/log.h
+++ pandora/src/core/log.h
@@ -7,17 +7,19 @@
 
 namespace Pandora {
 
-	class PANDORA_API Log
+
+	// NOTE: When we add API macro on the class we get dll-interface warnings
+	class Log
 	{
 	public:
-		static void Init();
+		static PANDORA_API void Init();
 
-		inline static std::shared_ptr<spdlog::logger> GetCoreLogger() { return s_CoreLogger; }
-		inline static std::shared_ptr<spdlog::logger> GetClientLogger() { return s_ClientLogger; }
+		inline static PANDORA_API std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static PANDORA_API std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 
 	private:
-		static std::shared_ptr<spdlog::logger> s_CoreLogger;
-		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+		static PANDORA_API std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static PANDORA_API std::shared_ptr<spdlog::logger> s_ClientLogger;
 	};
 
 }
